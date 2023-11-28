fcbd6bec1ed718d527224ce188cb5eb62a8d48c1
Granth Mangukiya <granth0407@gmail.com>
2023-11-28 06:05:42 +0530

    Test 3 (loop)

---
+++ test3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("the first ten natural numbers are:\n");
+
+    for (int i = 1; i < 11; i++)
+    {
+        printf("%d\n", i);
+    }
+    
+    return 0;
+}

5d89aa0f59075cfd469901fb5641fb5b8245b55a
Granth Mangukiya <granth0407@gmail.com>
2023-12-01 06:52:52 +0530

    #27 over

---
+++ 27.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    // char a = '3';
+    // char *ptra = &a;
+    // printf("%d\n", ptra);
+    // printf("%d\n", ptra + 2);
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 67};
+    printf("Value at position 3 of array is:%d\n", arr[3]);
+    printf("%d", arr[3]);
+
+    return 0;
+}
